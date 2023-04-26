1ade752ec8c1f3034086d9e7c6785c338febf1c2
replayzor <oltean.ionut1989@gmail.com>
2023-04-26 13:57:21 +0300

    remove unused dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Carousel from "./components/Carousel";
-import { ToastContainer, toast } from "react-toastify";
 
 function App() {
 	return (
 		<main>
 			<Carousel />
-			<ToastContainer toast={toast} />
 		</main>
 	);
 }
